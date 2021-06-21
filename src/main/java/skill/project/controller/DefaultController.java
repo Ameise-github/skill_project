70c10e5c6be37f207b17e30effbd4854c15d8cc1
AlexPan <panina.Alexa5@gmail.com>
2021-06-21 22:46:41 +0300

    initial commit

---
+++ src/main/java/skill/project/controller/DefaultController.java
@@ -0,0 +1,16 @@
+package skill.project.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DefaultController {
+
+  @GetMapping
+  public String start(Model model){
+    return "index.html";
+  }
+}
